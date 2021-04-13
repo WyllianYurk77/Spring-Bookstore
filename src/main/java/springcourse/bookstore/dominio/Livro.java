c4a687a2263718f32cac998855c81b5740d1ecc1
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-13 00:02:40 -0300

    Criando as classes de dominio

---
+++ src/main/java/springcourse/bookstore/dominio/Livro.java
@@ -0,0 +1,87 @@
+package springcourse.bookstore.dominio;
+
+public class Livro {
+    private Integer id;
+    private String title;
+    private String author;
+    private String text;
+    private Categoria category;
+
+    public Livro() {
+        super();
+    }
+
+    public Livro(Integer id, String title, String author, String text, Categoria category) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.text = text;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Categoria getCategory() {
+        return category;
+    }
+
+    public void setCategory(Categoria category) {
+        this.category = category;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
