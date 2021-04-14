bbf32dfc5f43a7497b64bc7f9bad1318da318571
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-14 00:35:27 -0300

    Criando o perfil de configuração de testes

---
+++ src/main/java/springcourse/bookstore/config/TestConfig.java
@@ -0,0 +1,21 @@
+package springcourse.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import springcourse.bookstore.servico.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void initiateDatabase() {
+        this.dbService.initiateDatabase();
+    }
+}
