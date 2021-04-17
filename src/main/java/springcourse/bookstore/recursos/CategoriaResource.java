627e8c50283dc405b5c13c5f3ea9decdab0180db
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-17 15:56:14 -0300

    Criando método para criação de nova categoria no endpoint

--- src/main/java/springcourse/bookstore/recursos/CategoriaResource.java
+++ src/main/java/springcourse/bookstore/recursos/CategoriaResource.java
@@ -1,5 +1,6 @@
 package springcourse.bookstore.recursos;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import springcourse.bookstore.dominio.Categoria;
 import springcourse.bookstore.dtos.CategoriaDTO;
@@ -32,5 +36,12 @@ public class CategoriaResource {
         List<Categoria> list01 = catService.findAll();
         List<CategoriaDTO> list02 = list01.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(list02);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> createCategory(@RequestBody Categoria cat) {
+        cat = catService.createCategory(cat);
+        URI identifier = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cat.getId()).toUri();
+        return ResponseEntity.created(identifier).build();
     }
 }
