8313bdbb9d87e8000fe6ec2236d30a8a0ef941ee
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-17 15:25:37 -0300

    Criando método para mostrar todas as categorias no endpoint

--- src/main/java/springcourse/bookstore/recursos/CategoriaResource.java
+++ src/main/java/springcourse/bookstore/recursos/CategoriaResource.java
@@ -1,5 +1,8 @@
 package springcourse.bookstore.recursos;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import springcourse.bookstore.dominio.Categoria;
+import springcourse.bookstore.dtos.CategoriaDTO;
 import springcourse.bookstore.servico.CategoriaService;
 
 @RestController
@@ -21,5 +25,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
         Categoria object = catService.findById(id);
         return ResponseEntity.ok().body(object);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list01 = catService.findAll();
+        List<CategoriaDTO> list02 = list01.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(list02);
     }
 }
