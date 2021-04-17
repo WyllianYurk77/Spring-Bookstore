9a1af29199eb99ad19ad291e6083d8f4ea6f3020
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-17 15:24:41 -0300

    Criando método para listar todas as categorias

--- src/main/java/springcourse/bookstore/servico/CategoriaService.java
+++ src/main/java/springcourse/bookstore/servico/CategoriaService.java
@@ -1,5 +1,6 @@
 package springcourse.bookstore.servico;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class CategoriaService {
         Optional<Categoria> object = categoryRepository.findById(id);
         return object.orElseThrow(() -> new ObjectNotFoundException(
                 "Object not found! ID: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public List<Categoria> findAll() {
+        return categoryRepository.findAll();
     }
 }
