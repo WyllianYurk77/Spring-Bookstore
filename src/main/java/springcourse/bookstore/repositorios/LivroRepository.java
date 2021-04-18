9a8837546715988b0613b80a4a7b69bd02ba318c
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-17 22:27:00 -0300

    Criando o método para pegar todos os livros pelo ID de sua categoria

--- src/main/java/springcourse/bookstore/repositorios/LivroRepository.java
+++ src/main/java/springcourse/bookstore/repositorios/LivroRepository.java
@@ -1,11 +1,18 @@
 package springcourse.bookstore.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import springcourse.bookstore.dominio.Livro;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+    @Query("SELECT obj FROM Livro obj WHERE obj.category.id = :idCat ORDER BY title")
+    List<Livro> findAllByCategory(@Param(value = "idCat") Integer idCat);
     
 }
