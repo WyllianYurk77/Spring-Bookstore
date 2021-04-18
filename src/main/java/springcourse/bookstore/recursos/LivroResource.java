2d74c1b0fd2863e21a519e3f0b3663038e357cce
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-18 17:23:31 -0300

    Criando método para adição de novo livro no endpoint

--- src/main/java/springcourse/bookstore/recursos/LivroResource.java
+++ src/main/java/springcourse/bookstore/recursos/LivroResource.java
@@ -1,5 +1,6 @@
 package springcourse.bookstore.recursos;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import springcourse.bookstore.dominio.Livro;
 import springcourse.bookstore.dtos.LivroDTO;
@@ -21,7 +24,7 @@ import springcourse.bookstore.servico.LivroService;
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
-    
+
     @Autowired
     private LivroService bookServ;
 
@@ -32,10 +35,18 @@ public class LivroResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<LivroDTO>> findAllByCategory(@RequestParam(value = "categoria", defaultValue = "0") Integer idCat) {
+    public ResponseEntity<List<LivroDTO>> findAllByCategory(
+            @RequestParam(value = "categoria", defaultValue = "0") Integer idCat) {
         List<Livro> list01 = bookServ.findAllByCategory(idCat);
         List<LivroDTO> list02 = list01.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(list02);
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> addBook(@RequestParam(value = "categoria", defaultValue = "0") Integer idCat, @RequestBody Livro book) {
+        Livro newBook = bookServ.addBook(idCat, book);
+        URI identifier = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newBook.getId()).toUri();
+        return ResponseEntity.created(identifier).build();
     }
 
     @PutMapping(value = "/{id}")
