653b0a2d4ffdddc1a11658ca2b30db3145f9e63f
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-17 22:29:05 -0300

    Criando método para mostrar todos os livros por categoria no endpoint

--- src/main/java/springcourse/bookstore/recursos/LivroResource.java
+++ src/main/java/springcourse/bookstore/recursos/LivroResource.java
@@ -1,13 +1,18 @@
 package springcourse.bookstore.recursos;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import springcourse.bookstore.dominio.Livro;
+import springcourse.bookstore.dtos.LivroDTO;
 import springcourse.bookstore.servico.LivroService;
 
 @RestController
@@ -21,5 +26,12 @@ public class LivroResource {
     public ResponseEntity<Livro> findById(@PathVariable Integer id) {
         Livro boo = bookServ.findById(id);
         return ResponseEntity.ok().body(boo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroDTO>> findAllByCategory(@RequestParam(value = "categoria", defaultValue = "0") Integer idCat) {
+        List<Livro> list01 = bookServ.findAllByCategory(idCat);
+        List<LivroDTO> list02 = list01.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(list02);
     }
 }
