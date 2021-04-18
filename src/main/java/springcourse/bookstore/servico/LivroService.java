302bedc96e88a4a2536feb5655583decdb215198
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-17 22:27:55 -0300

    Criando o método de listar todos os livros por categoria

--- src/main/java/springcourse/bookstore/servico/LivroService.java
+++ src/main/java/springcourse/bookstore/servico/LivroService.java
@@ -1,5 +1,6 @@
 package springcourse.bookstore.servico;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,17 @@ public class LivroService {
     @Autowired
     private LivroRepository bookRepo;
 
+    @Autowired
+    private CategoriaService categoryServ;
+
     public Livro findById(Integer id) {
         Optional<Livro> object = bookRepo.findById(id);
         return object.orElseThrow(() -> new ObjectNotFoundException(
             "Object not found! ID: " + id + ", Tipo: " + Livro.class.getName()));
+    }
+
+    public List<Livro> findAllByCategory(Integer id) {
+        categoryServ.findById(id);
+        return bookRepo.findAllByCategory(id);
     }
 }
