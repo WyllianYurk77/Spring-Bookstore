682a968f0e962c736328f262b7e3a2036bdab3a9
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-18 00:30:36 -0300

    Criando métodos para atualização de novo livro no endpoint

--- src/main/java/springcourse/bookstore/recursos/LivroResource.java
+++ src/main/java/springcourse/bookstore/recursos/LivroResource.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +36,17 @@ public class LivroResource {
         List<Livro> list01 = bookServ.findAllByCategory(idCat);
         List<LivroDTO> list02 = list01.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(list02);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro book) {
+        Livro newBook = bookServ.update(id, book);
+        return ResponseEntity.ok().body(newBook);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Livro> simpleUpdate(@PathVariable Integer id, @RequestBody Livro book) {
+        Livro newBook = bookServ.update(id, book);
+        return ResponseEntity.ok().body(newBook);
     }
 }
